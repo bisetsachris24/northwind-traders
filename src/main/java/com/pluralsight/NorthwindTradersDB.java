7b2cf30f284bf0fcd021a5660b257bb95af3d2e1
amanichristianbisetsa <bisetsachristian@gmail.com>
2026-06-05 12:38:08 -0500

    feat: implement product listing from Northwind database
    - Configure BasicDataSource connection
    - Execute SELECT query on Products table
    - Retrieve product names from ResultSet
    - Display product names in console

--- src/main/java/com/pluralsight/NorthwindTradersDB.java
+++ src/main/java/com/pluralsight/NorthwindTradersDB.java
@@ -7,7 +7,7 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
-public class NortwindTradersDB {
+public class NorthwindTradersDB {
     public static void main(String[] args) {
 
         BasicDataSource dataSource = new BasicDataSource();
